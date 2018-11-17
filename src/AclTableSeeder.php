1d2154673269e694b5c24d2ded8693b5d7f4f54b
Tim Joosten <Tjoosten3@gmail.com>
2018-11-17 02:12:15 +0100

    Implement AclTableSeeder

--- src/AclTableSeeder.php
+++ src/AclTableSeeder.php
@@ -27,7 +27,89 @@ class AclTableSeeder extends DatabaseSeeder
             $inputRoles = $this->command->ask('Enter roles in comma separated formal.', 'admin, user');
             $this->createRoleIfNotExists($inputRoles);
         } else { // Only default user is needed
-            $this->createOnlyNormalUser();
+            $this->createOnlyNormalUserRole();
         }
+    }
+
+    /**
+     * Implement the default permissions in the storage for the application.
+     * 
+     * @return void
+     */
+    protected function seedDefaultPermissions(): void 
+    {
+        foreach ($this->defaultPermissions() as $permission) {
+            Permission::firstOrCreate(['name' => trim($permission)]);
+        }
+    }
+
+    /**
+     * Function for creating only an normal user role in the application. 
+     * 
+     * @return void 
+     */
+    protected function createOnlyNormalUserRole(): void 
+    {
+        Role::firstOrCreate(['name' => 'user']);
+        $this->command->info('Added only default user role.');
+    }
+
+    /**
+     * Function whe we define the default permissions for the application. 
+     * 
+     * @return void
+     */
+    public function defaultPermissions(): array 
+    {
+        // Here we can define default permissions for the application. 
+        // For this application there are no default permissions for now. 
+        // Because we only use the roles section from spatie/permission package.
+
+        return [];
+    }
+
+    /**
+     * Function for attaching permissions and creating a role if it not already exists in the database. 
+     * 
+     * @param  string $roles The one dimensional array for the given roles.
+     * @return void
+     */
+    protected function createRoleIfNotExist(string $roles): void 
+    {
+        foreach (explode(',', $roles) as $role) {
+            $role = Role::firstOrCreate(['name' => trim($role)]);
+            if ($this->isAdmin($role->name)) { // Assign all permissions
+                $role->syncPermissions(Permission::all());
+                $this->command->info('Admin granted all permissions');
+            } else { // For others by default only read access
+                $role->syncPermissions($this->getUserPermissions());
+            }
+            $this->createUser($role); // Create one user for each role.
+        }
+    }
+    
+    /**
+     * Creating a normal user in the storage
+     * 
+     * @return void
+     */
+    protected function createUser(Role $role): void 
+    {
+        $user = factory(User::class)->create(['password' => 'secret'])->assignRole($role->name);
+        if ($this->isAdmin($role->name)) {
+            $this->command->info('Here are your admin details to login:'); 
+            $this->command->warn($user->email); 
+            $this->command->warn('Password is "secret"');
+        }
+    }
+
+    /**
+     * Determine if the created role is admin or not. 
+     * 
+     * @return bool
+     */
+    protected function isAdmin(string $role): bool 
+    {
+        return $role === 'admin'; 
     }
 }
